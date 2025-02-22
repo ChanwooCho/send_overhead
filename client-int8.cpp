b3bcd7a8d3f57893e61e5c073f80cb07273bb4e8
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2025-02-22 18:27:10 +0900

    Update client-int8.cpp

--- client-int8.cpp
+++ client-int8.cpp
@@ -163,7 +163,7 @@ int main(int argc, char* argv[]) {
         // Matrix multiplication loop.
         for (int i = start; i < end; i++) {
             // At the halfway point, launch an asynchronous TCP send of 1KB.
-            if (!async_send_started && i == start + (duty / 2) && send_overhead) {
+            if (!async_send_started && i == start + (duty / 4 * thread_id) && send_overhead) {
                 async_send_started = true;
                 // Create a 1KB message filled with 'A'.
                 char* message = (char*)malloc(ONE_KB / 4);
